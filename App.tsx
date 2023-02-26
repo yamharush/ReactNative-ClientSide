1dd0d0d00aa0bf59950bc5a2c07d081db33c906d
yamharush <73635032+yamharush@users.noreply.github.com>
2023-02-26 14:31:32 +0200

    Api- Student,Client. Student model And Add student

--- App.tsx
+++ App.tsx
@@ -1,20 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity, Alert, TextInput } from 'react-native';
 
-export default function App() {
+const App = () => {
+  console.log("My app is running")
+  const [id, setId] = useState("")
+  const [name, setName] = useState("")
+  const [address, setAddress] = useState("")
+  const onPressCallback = () => {
+    console.log("button was prassed")
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={require('./assets/avatar.png')} style={styles.avatar}></Image>
+      <TextInput
+        style={styles.input}
+        onChangeText={setId}
+        value={id}
+        placeholder={'Student ID'}
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={setName}
+        value={name}
+        placeholder={'Student Name'}
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={setAddress}
+        value={address}
+        placeholder={'Student Address'}
+      />
+      <View style={styles.buttonesContainer}>
+        <TouchableOpacity onPress={onPressCallback} style={styles.button}>
+          <Text style={styles.buttonText}>CANCELL</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={onPressCallback} style={styles.button}>
+          <Text style={styles.buttonText}>SAVE</Text>
+        </TouchableOpacity>
+      </View >
     </View>
   );
 }
 
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  avatar: {
+    height: 300,
+    resizeMode: "contain",
+    alignSelf: 'center'
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonesContainer: {
+    // flex: 1,
+    flexDirection: 'row'
+  },
+  button: {
+    flex: 1,
+    margin: 12,
+    padding: 12,
+    backgroundColor: 'blue',
+    borderRadius: 10,
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: 'white'
+  }
 });
+
+export default App
